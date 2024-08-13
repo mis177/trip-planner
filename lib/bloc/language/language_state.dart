18d5d0577c880694ac3546d40086cdf6bd7ca01a
MichalJ <michal.jamroz@t2t-system.com>
2024-08-13 17:07:53 +0200

    FIXED overriding prop in LanguageState

--- lib/bloc/language/language_state.dart
+++ lib/bloc/language/language_state.dart
@@ -4,17 +4,13 @@ sealed class LanguageState extends Equatable {
   final String localeName;
   const LanguageState({required this.localeName});
   @override
-  List<Object?> get props => [];
+  List<Object?> get props => [localeName];
 }
 
 class LanguageInitial extends LanguageState {
   const LanguageInitial({required super.localeName});
-  @override
-  List<Object?> get props => [localeName];
 }
 
 class LanguageChanged extends LanguageState {
   const LanguageChanged({required super.localeName});
-  @override
-  List<Object?> get props => [localeName];
 }
