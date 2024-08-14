a2aaa88238931b09bec4173abc5dd7c8020bc99c
MichalJ <michal.jamroz@t2t-system.com>
2024-08-14 11:59:19 +0200

    CHANGED style of confirmation_dialog

--- lib/utilities/dialogs/confirmation_dialog.dart
+++ lib/utilities/dialogs/confirmation_dialog.dart
@@ -7,28 +7,45 @@ Future<bool?> showConfirmationDialog({
 }) {
   return showDialog<bool>(
     context: context,
+    barrierDismissible: false,
     builder: (context) {
       return AlertDialog(
-        title: Text(title),
-        content: Text(content),
+        title: Row(
+          children: [
+            Icon(Icons.warning, color: Theme.of(context).primaryColor),
+            const SizedBox(width: 10),
+            Text(title),
+          ],
+        ),
+        content: Text(
+          content,
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.grey[800],
+          ),
+        ),
         actions: [
-          TextButton(
+          TextButton.icon(
             style: TextButton.styleFrom(
+              foregroundColor: Colors.red,
               textStyle: Theme.of(context).textTheme.labelLarge,
             ),
             onPressed: () {
               Navigator.of(context).pop(false);
             },
-            child: const Text('No'),
+            icon: const Icon(Icons.cancel),
+            label: const Text('No'),
           ),
-          TextButton(
+          TextButton.icon(
             style: TextButton.styleFrom(
+              foregroundColor: Theme.of(context).primaryColor,
               textStyle: Theme.of(context).textTheme.labelLarge,
             ),
             onPressed: () {
               Navigator.of(context).pop(true);
             },
-            child: const Text('Yes'),
+            icon: const Icon(Icons.check_circle),
+            label: const Text('Yes'),
           ),
         ],
       );
